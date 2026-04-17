d4e20c724b908b6518e06db732fcef4184c373c9
Brian Grefenstette <bwgref@srl.caltech.edu>
2026-04-17 05:04:34 -0700

    Cleanup of old bootstrap residuals
    Cleanup

--- nustar_gen/__init__.py
+++ nustar_gen/__init__.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
 # Enforce Python version check during package import.
 # This is the same check as the one at the top of setup.py
 import sys
@@ -17,8 +11,3 @@ class UnsupportedPythonError(Exception):
 
 if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
     raise UnsupportedPythonError("astroduet does not support Python < {}".format(__minimum_python_version__))
-
-# For egg_info test builds to pass, put package imports here.
-#from .utils import *
-#from .wrappers import *
-#    from .filters import *
